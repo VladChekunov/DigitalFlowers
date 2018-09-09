9e46e50cc774702993a721d1ec1d2993648b3d9a
Vlad Chekunov <vladlenchekunov@yandex.com>
2018-09-09 10:53:18 +0300

    add products frontend

---
+++ admin/db4init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `mydb`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `login` VARCHAR(24) NOT NULL,
+  `pass` VARCHAR(32) NOT NULL,
+  `key` VARCHAR(32) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
---
+++ admin/getpage.php
@@ -0,0 +1,6 @@
+<?
+/*
+Render specific page with footer header etc
+*/
+
+?>
